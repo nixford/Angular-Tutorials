b4f9a0f3550bb9140b0e16c2b319eb60aa31dbbc
Dimitar Uzunov <63246090+nixford@users.noreply.github.com>
2021-10-13 15:01:51 +0300

    add routing module

--- Angular-Demo-Forms/src/app/app-routing.module.ts
+++ Angular-Demo-Forms/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsTemplateDrivenComponent } from './forms-template-driven/forms-template-driven.component';
+import { FormsReactiveComponent } from './forms-reactive/forms-reactive.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'template', component: FormsTemplateDrivenComponent },
+  { path: 'reactive', component: FormsReactiveComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
